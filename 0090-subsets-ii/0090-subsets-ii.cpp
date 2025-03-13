465ba61d0bb31c9a27379b609875d74fc1dcf56e
7Navneet7 <navneetsingh19171425@gmail.com>
2025-03-13 22:17:29 +0530

    Time: 3 ms (38.79%), Space: 10.6 MB (49.49%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -18,11 +18,29 @@ public:
             ds.pop_back();
         }
     }
+
+
+    void uniqueSubsets2(int ind,vector<int>&nums,set<vector<int>>&ans,vector<int>&ds){
+        int n =nums.size();
+        if(ind==n){
+            
+            ans.insert(ds);
+            return;
+        }
+        ds.push_back(nums[ind]);
+        uniqueSubsets2(ind+1,nums,ans,ds);
+        ds.pop_back();
+        uniqueSubsets2(ind+1,nums,ans,ds);
+    }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>>ans;
+        set<vector<int>>ans;
         vector<int>ds;
         sort(nums.begin(),nums.end());
-        uniqueSubsets(0,nums,ans,ds);
-        return ans;
+        uniqueSubsets2(0,nums,ans,ds);
+        vector<vector<int>>ans2;
+        for(auto it=ans.begin();it!=ans.end();it++){
+            ans2.push_back(*it);
+        }
+        return ans2;
     }
 };
