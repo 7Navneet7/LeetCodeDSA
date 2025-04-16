b86c43166c9cbc6261e919b687670c12fec41200
7Navneet7 <navneetsingh19171425@gmail.com>
2025-04-16 22:29:52 +0530

    Time: 2532 ms (16.80%) | Memory: 98.3 MB (63.03%) - LeetSync

---
+++ 3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+
+
+    // void solve(int ind,int t,vector<int>&nums,set<int>&xo,int op){
+    //     if(t==3){
+    //         xo.insert(op);return;
+    //     } 
+
+    //     if (ind >= nums.size() || nums.size() - ind <3-t-1) return;
+        
+    //     // if(ind==nums.size()){
+            
+    //     //     return;
+    //     // }
+    //     for(int i=ind;i<nums.size();i++){
+    //         if(i>ind && nums[i]==nums[i-1])continue;
+    //         if(t>2)break;
+    //         op=op^nums[i];
+    //         solve(i,t+1,nums,xo,op);
+    //         op=op^nums[i];
+            
+    //     }
+    //     // if(t<3){
+    //     //     op^=nums[ind];
+    //     //     solve(ind,t+1,nums,xo,op);
+    //     //     op^=nums[ind];
+    //     // }
+    //     // solve(ind+1,t,nums,xo,op);
+        
+    // }
+    // int uniqueXorTriplets(vector<int>& nums) {
+    //     if(nums.size()<3)return nums.size();
+    //     int op=0;set<int>xo;
+    //     solve(0,0,nums,xo,op);
+    //     return xo.size();
+    // }
+
+
+    int uniqueXorTriplets(vector<int>& nums){
+        int n=nums.size();unordered_set<int>st1,st2;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                st1.insert(nums[i]^nums[j]);
+            }
+        }
+        for(int x:st1){
+            for(int j=0;j<n;j++){
+                st2.insert(x ^ nums[j]);
+            }
+        }
+        return st2.size();
+    }
+};
