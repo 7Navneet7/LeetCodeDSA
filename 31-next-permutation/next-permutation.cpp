71f7936e6e35a03eae82e10df5337d24ed2a03b7
7Navneet7 <navneetsingh19171425@gmail.com>
2025-05-07 19:53:14 +0530

    Time: 0 ms (100.00%) | Memory: 15.9 MB (32.74%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -23,27 +23,27 @@ public:
 
 
     void nextPermutation(vector<int>& nums) {
-        int n=nums.size();
-        int ind=-1;
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                ind=i;break;
-            }
-        }
-        if(ind==-1){
-            reverse(nums.begin(),nums.end());
-            return ;
-        }
-        //int mx=max_element(nums.begin()+ind,nums.end())-nums.begin();
-        for(int i=n-1;i>ind;i--){
-            if(nums[ind]<nums[i]){
-                swap(nums[ind],nums[i]);break;
-            }
-        }
-        reverse(nums.begin()+ind+1,nums.end());
+    //     int n=nums.size();
+    //     int ind=-1;
+    //     for(int i=n-2;i>=0;i--){
+    //         if(nums[i]<nums[i+1]){
+    //             ind=i;break;
+    //         }
+    //     }
+    //     if(ind==-1){
+    //         reverse(nums.begin(),nums.end());
+    //         return ;
+    //     }
+    //     //int mx=max_element(nums.begin()+ind,nums.end())-nums.begin();
+    //     for(int i=n-1;i>ind;i--){
+    //         if(nums[ind]<nums[i]){
+    //             swap(nums[ind],nums[i]);break;
+    //         }
+    //     }
+    //     reverse(nums.begin()+ind+1,nums.end());
         
       
-        //for(auto &it:nums)cout<<it;
-    }    
-
+    //     //for(auto &it:nums)cout<<it;
+        next_permutation(nums.begin(),nums.end());
+     }    
 };
