4a224a5be033608cec08ef821f144ce610fb9457
7Navneet7 <navneetsingh19171425@gmail.com>
2025-06-01 00:00:14 +0530

    Time: 12 ms (45.46%) | Memory: 19.6 MB (23.71%) - LeetSync

--- 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.cpp
+++ 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
+    // int minOperations(vector<int>& nums) {
+    //     int ans=0;
+    //     int n=nums.size();
+    //     for(int i=1;i<n;i++){
+    //         if(nums[i]<=nums[i-1]){
+    //             ans+=nums[i-1]+1-nums[i];
+    //             nums[i]=nums[i-1]+1;
+    //         }
+    //     }
+    //     return ans;
+        
+    // }
+
     int minOperations(vector<int>& nums) {
-        int ans=0;
-        int n=nums.size();
+        int n=nums.size();int ops=0;int k;
         for(int i=1;i<n;i++){
-            if(nums[i]<=nums[i-1]){
-                ans+=nums[i-1]+1-nums[i];
+            if(nums[i]>nums[i-1])continue;
+            else{
+                k=nums[i];
                 nums[i]=nums[i-1]+1;
+                ops+=(nums[i]-k);
             }
         }
-        return ans;
-        
-    }
+        return ops;
+    }    
 };
