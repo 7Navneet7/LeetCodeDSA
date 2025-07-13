314f799fc8e712a29a46056b42c5892329ec46ae
7Navneet7 <navneetsingh19171425@gmail.com>
2025-07-13 10:23:59 +0530

    Time: 3 ms (31.65%) | Memory: 23.1 MB (5.09%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        intervals.push_back(newInterval);
-        sort(intervals.begin(), intervals.end());
-
-        vector<vector<int>> res;
-        res.push_back(intervals[0]);
-
-        for (int i = 1; i < intervals.size(); ++i) {
-            if (res.back()[1] >= intervals[i][0]) {
-                res.back()[1] = max(res.back()[1], intervals[i][1]);
-            } else {
-                res.push_back(intervals[i]);
+    vector<vector<int>> merge(vector<vector<int>>& nums) {
+        int n=nums.size();vector<vector<int>>ans;
+        sort(nums.begin(),nums.end());
+        ans.push_back(nums[0]);
+        for(int i=1;i<n;i++){
+            if(ans.back()[1]<nums[i][0]){ans.push_back(nums[i]);}
+            else{
+                ans.back()[1]=max(nums[i][1],ans.back()[1]);
             }
         }
-
-        return res;        
+        return ans;
+    }
+    vector<vector<int>> insert(vector<vector<int>>& nums, vector<int>& newnum) {
+        int n=nums.size();
+        if(n==0)return{newnum};
+        for(int i=0;i<n;i++){
+            if(nums[i][1]>=newnum[0] && nums[i][0]<=newnum[0]){
+                nums[i][1]=max(nums[i][1],newnum[1]);
+                nums[i][0]=min(nums[i][0],newnum[0]);break;
+            }
+            else{nums.push_back(newnum);}
+        }
+        return merge(nums);
     }
 };
