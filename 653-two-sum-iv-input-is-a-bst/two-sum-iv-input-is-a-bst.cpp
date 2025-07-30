e3d2621caa8cf4faa2b3e35501af76bbcac98593
7Navneet7 <navneetsingh19171425@gmail.com>
2025-07-30 23:19:02 +0530

    Time: 10 ms (18.47%) | Memory: 38.9 MB (20.35%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -14,8 +14,8 @@ public:
     set<int>st;
     bool findTarget(TreeNode* root, int k) {
         if(root==NULL)return false;
-        //if(st.count(k-root->val))return true;
-        if(st.find(k-root->val)!=st.end())return true; 
+        if(st.count(k-root->val))return true;
+        //if(st.find(k-root->val)!=st.end())return true; 
         st.insert(root->val);
         return (findTarget(root->left,k)||findTarget(root->right,k));
     }
