2e78aaa6c234a80f9d317d6247456be69918873c
7Navneet7 <navneetsingh19171425@gmail.com>
2025-07-30 10:19:30 +0530

    Time: 0 ms (100.00%) | Memory: 59.4 MB (45.31%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.cpp
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.cpp
@@ -11,38 +11,24 @@
  */
 class Solution {
 public:
-    TreeNode* Ceil(TreeNode* root,int val){
-        TreeNode* node=nullptr;
-        while(root!=nullptr){
-            if(root->val==val)return root;
-            if(root->val>val){
-                node=root;
-                root=root->left;
-            }
-            else root=root->right;
-        }
-        return node;
-    }
-    TreeNode* Floor(TreeNode* root,int val){
-        TreeNode* node=nullptr;
-        while(root!=nullptr){
-            if(root->val==val)return root;
-            if(root->val<val){
-                node=root;root=root->right;
-            }
-            else root=root->left;
-        }
-        return node;
-    }
     TreeNode* insertIntoBST(TreeNode* root, int val) {
         TreeNode* node=new TreeNode(val);
-        TreeNode* ceil=Ceil(root,val);
-        TreeNode* floor=Floor(root,val);
-        if(ceil!=nullptr && ceil->left==nullptr)ceil->left=node;
-        //else if(ceil->right==nullptr)ceil->right=node;
-        //else if(floor->left==nullptr)floor->left=node;
-        else if(floor!=nullptr && floor->right==nullptr) floor->right=node;
-        else return node;
+        TreeNode* curr=root;
+        if(root==NULL)return node;
+        while(true){
+            if(curr->val>val){
+                if(curr->left)curr=curr->left;
+                else{
+                    curr->left=node;break;
+                }
+            }
+            else{
+                if(curr->right)curr=curr->right;
+                else{
+                    curr->right=node;break;
+                }
+            }
+        }  
         return root;
     }
 };
