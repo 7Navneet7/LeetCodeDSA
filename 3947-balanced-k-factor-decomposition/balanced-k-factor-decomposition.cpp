155829b6fe8d93e6327d421ee8875058b53ea8b0
7Navneet7 <navneetsingh19171425@gmail.com>
2025-09-10 09:23:22 +0530

    Time: 67 ms (42.45%) | Memory: 13.3 MB (27.79%) - LeetSync

--- 3947-balanced-k-factor-decomposition/balanced-k-factor-decomposition.cpp
+++ 3947-balanced-k-factor-decomposition/balanced-k-factor-decomposition.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
-    vector<int> best;
-    int bestDiff = INT_MAX;
-
+    vector<int>best;int bestDiff=INT_MAX;
     vector<int> minDifference(int n, int k) {
-        vector<int> curr;
-        backtrack(n, k, 1, curr);
+        vector<int>curr;
+        backTrack(n,k,1,curr);
         return best;
     }
-
-    void backtrack(int n, int k, int start, vector<int>& curr) {
-        if (k == 1) {
-            if (n >= start) {
-                curr.push_back(n);
-                check(curr);
-                curr.pop_back();
-            }
+    void backTrack(int n,int k,int st,vector<int>&curr){
+        if(k==1 ){
+            //if(n>=st){
+            curr.push_back(n);
+            check(curr);
+            curr.pop_back();
+            //}
             return;
         }
-
-        for (int i = start; i <= n; i++) {
-            if (n % i == 0) {
+        for(int i=st;i<=n;i++){
+            if(n%i==0){
                 curr.push_back(i);
-                backtrack(n / i, k - 1, i, curr);
+                backTrack(n/i,k-1,i,curr);
                 curr.pop_back();
             }
         }
     }
-
-    void check(vector<int>& curr) {
-        int mn = *min_element(curr.begin(), curr.end());
-        int mx = *max_element(curr.begin(), curr.end());
-        if (mx - mn < bestDiff) {
-            bestDiff = mx - mn;
-            best = curr;
+    void check(vector<int>curr){
+        int mx=*max_element(curr.begin(),curr.end());
+        int mn=*min_element(curr.begin(),curr.end());
+        if(mx-mn<bestDiff){
+            bestDiff=mx-mn;best=curr;
         }
     }
 };
