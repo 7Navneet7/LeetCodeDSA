44ce8ce52f7756b644f334e859b10975a47562ee
7Navneet7 <navneetsingh19171425@gmail.com>
2025-09-23 23:47:38 +0530

    Time: 0 ms (100%), Space: 94.9 MB (93.69%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n=nums1.size(),m=nums2.size();
-        vector<int>ans;
-        int i=0,j=0;
-        while(i<n&&j<m){
-            if(nums1[i]<=nums2[j]){ans.push_back(nums1[i]);i++;}
-            else{ans.push_back(nums2[j++]);}
+        int n = nums1.size();
+        int m = nums2.size();
+        int i = 0, j = 0, m1 = 0, m2 = 0;
+
+        // Find median.
+        for (int count = 0; count <= (n + m) / 2; count++) {
+            m2 = m1;
+            if (i != n && j != m) {
+                if (nums1[i] > nums2[j]) {
+                    m1 = nums2[j++];
+                } else {
+                    m1 = nums1[i++];
+                }
+            } else if (i < n) {
+                m1 = nums1[i++];
+            } else {
+                m1 = nums2[j++];
+            }
         }
-        while(i<n)ans.push_back(nums1[i++]);
-        while(j<m)ans.push_back(nums2[j++]);
-        if((n+m)&1)return (double)ans[(n+m)/2];
-        return (double)(ans[(n+m)/2]+ans[(n+m)/2-1])/2;
+
+        // Check if the sum of n and m is odd.
+        if ((n + m) % 2 == 1) {
+            return static_cast<double>(m1);
+        } else {
+            double ans = static_cast<double>(m1) + static_cast<double>(m2);
+            return ans / 2.0;
+        }
     }
 };
