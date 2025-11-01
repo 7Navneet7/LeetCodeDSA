ca4fd4bd65399016ae1e6c31051e68b1102cd06a
7Navneet7 <navneetsingh19171425@gmail.com>
2025-11-01 23:29:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3729-count-distinct-subarrays-divisible-by-k-in-sorted-array/3729-count-distinct-subarrays-divisible-by-k-in-sorted-array.cpp
@@ -0,0 +1,20 @@
+    long long numGoodSubarrays(vector<int>& A, int k) {
+        unordered_map<int,long long> cnt;
+        cnt[0] = 1;
+        int pre = 0, n = A.size();
+        long long res = 0;
+        for (int a : A) {
+            pre = (pre + a) % k;
+            res += cnt[pre]++;
+        }
+        for (int i = 0; i < n; ) {
+            int j = i;
+            while (j < n && A[j] == A[i]) ++j;
+            int l = j - i;
+            for (int ll = 1; ll < l; ++ll)
+                if ((1LL * ll * A[i]) % k == 0)
+                    res -= (l - ll);
+            i = j;
+        }
+        return res;
+    }
