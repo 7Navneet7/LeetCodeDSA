1cd1bf21863847eaab4acb7ef4a3168d39415afb
7Navneet7 <navneetsingh19171425@gmail.com>
2025-12-13 23:56:35 +0530

    Time: 11 ms (90.48%), Space: 8.6 MB (91.1%) - LeetHub

--- 3770-largest-prime-from-consecutive-prime-sum/3770-largest-prime-from-consecutive-prime-sum.cpp
+++ 3770-largest-prime-from-consecutive-prime-sum/3770-largest-prime-from-consecutive-prime-sum.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool isPrime(int n){
-        if(n<=2)return false;
-        if(n%2==0)return false;
+        if(n<2)return true;
+        if(n%2==0)return n==2;
         for(int i=3;i*i<=n;i+=2){
             if(n%i==0)return false;
         }
