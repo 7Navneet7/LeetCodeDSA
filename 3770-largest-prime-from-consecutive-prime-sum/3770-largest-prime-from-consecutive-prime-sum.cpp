aaa5a298fa7e74f3948aa86acfce369773b85ee9
7Navneet7 <navneetsingh19171425@gmail.com>
2025-12-13 23:50:05 +0530

    Time: 6 ms (93.88%), Space: 8.7 MB (84.37%) - LeetHub

---
+++ 3770-largest-prime-from-consecutive-prime-sum/3770-largest-prime-from-consecutive-prime-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPrime(int n){
+        if(n<=2)return false;
+        if(n%2==0)return false;
+        for(int i=3;i*i<=n;i+=2){
+            if(n%i==0)return false;
+        }
+        return true;
+    }
+    int largestPrime(int n) {
+        int sum=2;
+        int res=0;
+        if(n>=2)res=2;
+        for(int i=3;i+sum<=n;i++){
+            if(isPrime(i)){
+                sum+=i;
+                if(sum<=n && isPrime(sum))res=sum;
+            }
+        }
+        return res;
+    }
+};
