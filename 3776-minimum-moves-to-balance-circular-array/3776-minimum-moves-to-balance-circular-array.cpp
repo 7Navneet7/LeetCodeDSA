98f6fa9a3cc232b76a947a91b484a1bba637ed47
7Navneet7 <navneetsingh19171425@gmail.com>
2025-12-17 10:59:57 +0530

    Time: 2 ms (75.18%), Space: 193.8 MB (88.91%) - LeetHub

--- 3776-minimum-moves-to-balance-circular-array/3776-minimum-moves-to-balance-circular-array.cpp
+++ 3776-minimum-moves-to-balance-circular-array/3776-minimum-moves-to-balance-circular-array.cpp
@@ -1,42 +1,19 @@
+using ll=long long;
 class Solution {
 public:
-    long long minMoves(vector<int>& balance) {
-        long long sum = 0;
-        int idx = -1;
-        int n = balance.size();
-
-        for(int i = 0; i < n; i++){
-            sum += balance[i];
-            if(balance[i] < 0) idx = i;
+    long long minMoves(vector<int>& bal) {
+        ll j=-1;ll n=bal.size();ll tot=0;
+        for(ll i=0;i<n;i++){
+            if(bal[i]<0)j=i;
+            tot+=bal[i];
         }
-
-        if(sum < 0) return -1;
-        if(idx == -1) return 0;
-
-        long long val = balance[idx];
-        int left = (idx - 1 + n) % n;
-        int right = (idx + 1) % n;
-        long long ans = 0;
-        int dist = 1;
-
-        while(val < 0){
-            if(balance[left] > 0){
-                long long take = min((long long)balance[left], -val);
-                val += take;
-                ans += take * dist;
-            }
-
-            if(val < 0 && balance[right] > 0){
-                long long take = min((long long)balance[right], -val);
-                val += take;
-                ans += take * dist;
-            }
-
-            left = (left - 1 + n) % n;
-            right = (right + 1) % n;
-            dist++;
+        if(j==-1)return 0;ll ans=0;
+        if(tot<0)return -1;
+        for(ll d=1;bal[j]<0;d++){
+            ll sto=bal[(j+d)%n]+bal[(j-d+n)%n];
+            ans+=fmin(-bal[j],sto)*d;
+            bal[j]+=sto;
         }
-
         return ans;
     }
 };
