585f6b63bbebf05214b45f34c3c5cf4592057499
7Navneet7 <navneetsingh19171425@gmail.com>
2026-01-07 10:58:33 +0530

    Time: 0 ms (100%), Space: 109.5 MB (30.1%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -1,24 +1,35 @@
-// C-array implement queue
-TreeNode* q[100000];// queue for TreeNode*
-int front=0, back=0;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    static int maxLevelSum(TreeNode* root) {
-        int idx=0, sum=INT_MIN;
-        front=back=0;// reset q
-        q[back++]=root;// push
-        int level=1;
-        for( ; front<back; level++){
-            int qz=back-front;
-            int curSum=0;
-            for (int i=0; i<qz; i++) {
-                TreeNode* Node=q[front++];// pop
-                curSum+=Node->val;
-                if (Node->left) q[back++]=Node->left;// push
-                if (Node->right) q[back++]=Node->right;// push
+    int maxLevelSum(TreeNode* root) {
+        if(!root)return 0;
+        int lev=1;int mx=INT_MIN;
+        queue<TreeNode*>q;
+        q.push(root);int mxLev=1;
+        while(!q.empty()){
+            int levsum=0;int n=q.size();
+            for(int i=0;i<n;i++){
+                TreeNode* node=q.front();
+                q.pop();
+                levsum+=node->val;
+                if(node->left)q.push(node->left);
+                if(node->right)q.push(node->right);
             }
-            if(curSum>sum) idx=level, sum=curSum;
-        } 
-        return idx;
+            if(levsum>mx){
+                mx=levsum;mxLev=lev;
+            }
+            lev++;
+        }
+        return mxLev;
     }
 };
