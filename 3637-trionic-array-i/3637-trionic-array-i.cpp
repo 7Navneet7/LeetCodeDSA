1fac932b584834b6a6ec87b9c8684ebf3c0b9ca7
7Navneet7 <navneetsingh19171425@gmail.com>
2026-02-04 11:28:30 +0530

    Time: 0 ms (100%), Space: 27.7 MB (70.12%) - LeetHub

--- 3637-trionic-array-i/3637-trionic-array-i.cpp
+++ 3637-trionic-array-i/3637-trionic-array-i.cpp
@@ -1,38 +1,33 @@
 
-// class Solution {
-// public:
-//     bool isTrionic(vector<int>& nums) {
-//         bool f1=0,f2=0;bool f3=0;
-//         int n=nums.size();
-//         for(int i=0;i<n-1;i++){
-//             if(nums[i]<nums[i+1]){
-//                 f1=1;
-//             }
-//             else if(f2 && f1 &&(nums[i]<nums[i+1])){
-//                 f3=1;
-//             }
-//             else if(nums[i]>nums[i+1])f2=1;
-
-//         }
-//         return (f1 & f2 & f3);
-//     }
-// };
 class Solution {
 public:
     bool isTrionic(vector<int>& nums) {
-        int n = nums.size(), i = 1;
-        while (i < n && nums[i - 1] < nums[i]) {
-            i++;
+        bool u0=0,u1=0,u2=0;
+        int st=0;
+        int n=nums.size();
+        for(int i=0;i<n-1;i++){
+            if(nums[i]==nums[i+1])return 0;
+            if(st==0){
+                if(nums[i]<nums[i+1])u0=1;
+                else{
+                    if(!u0)return 0;
+                    else{
+                        u1=1;st=1;
+                    }
+                }
+            }
+            else if(st==1){
+                if(nums[i]>nums[i+1])u1=1;
+                else{
+                    st=2;
+                    u2=1;
+                }
+            }
+            else{
+                if(nums[i]<nums[i+1])u2=1;
+                else return 0;
+            }
         }
-        int p = i - 1;
-        while (i < n && nums[i - 1] > nums[i]) {
-            i++;
-        }
-        int q = i - 1;
-        while (i < n && nums[i - 1] < nums[i]) {
-            i++;
-        }
-        int flag = i - 1;
-        return (p != 0) && (q != p) && (flag == n - 1 && flag != q);
+        return (u0&u1&u2);
     }
 };
