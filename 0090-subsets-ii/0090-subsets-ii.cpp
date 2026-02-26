ddf11bd1e52b120081d66ff438778006202b5243
7Navneet7 <navneetsingh19171425@gmail.com>
2026-02-26 11:26:12 +0530

    Time: 4 ms (14.7%), Space: 10.9 MB (25.72%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,46 +1,23 @@
 class Solution {
 public:
-
-    void uniqueSubsets(int ind,vector<int>&nums,vector<vector<int>>&ans,vector<int>&ds){
-        int n=nums.size();
-        
-        // if(ind==n){
-            
-        //     return;
-        // }
-        ans.push_back(ds);    
-        //ds.pop_back();
-        for(int i=ind;i<n;i++){
+    void util(int ind,vector<int>nums,vector<int>&ds,vector<vector<int>>&ans){
+        //if(ind==nums.size()){
+            ans.push_back(ds);
+        //}
+        for(int i=ind;i<nums.size();i++){
             if(i>ind && nums[i]==nums[i-1])continue;
             ds.push_back(nums[i]);
-            //ans.push_back(ds);
-            uniqueSubsets(i+1,nums,ans,ds);
+            util(i+1,nums,ds,ans);
             ds.pop_back();
+            //util(i+1,nums,ds,ans);
         }
-    }
-
-
-    void uniqueSubsets2(int ind,vector<int>&nums,set<vector<int>>&ans,vector<int>&ds){
-        int n =nums.size();
-        if(ind==n){
-            sort(ds.begin(),ds.end());
-            ans.insert(ds);
-            return;
-        }
-        ds.push_back(nums[ind]);
-        uniqueSubsets2(ind+1,nums,ans,ds);
-        ds.pop_back();
-        uniqueSubsets2(ind+1,nums,ans,ds);
-    }
+        
+    } 
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        set<vector<int>>ans;
         vector<int>ds;
         sort(nums.begin(),nums.end());
-        uniqueSubsets2(0,nums,ans,ds);
-        vector<vector<int>>ans2;
-        for(auto it=ans.begin();it!=ans.end();it++){
-            ans2.push_back(*it);
-        }
-        return ans2;
+        vector<vector<int>>ans;
+        util(0,nums,ds,ans);
+        return ans;
     }
 };
