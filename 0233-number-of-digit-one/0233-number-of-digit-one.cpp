a839919562ccb3d1fae4d83c6ad0e7b6982bfaa7
7Navneet7 <navneetsingh19171425@gmail.com>
2026-03-02 23:39:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0233-number-of-digit-one/0233-number-of-digit-one.cpp
+++ 0233-number-of-digit-one/0233-number-of-digit-one.cpp
@@ -1,17 +1,35 @@
 using ll=long long;
 class Solution {
 public:
-    int countDigitOne(int n) {
-        ll fact=1,cnt=0;
-        while(fact<=n){
-            ll h=(n/(fact*10));
-            ll c=(n/fact)%10;
-            ll l=(n%fact);
-            if(c==0)cnt+=(h*fact);
-            else if(c==1)cnt+=(h*fact)+l+1;
-            else cnt+=((h+1)*fact);
-            fact*=10;
+    int dp[12][2];bool vis[12][2];
+    string s;
+    int dfs(int pos,bool t){
+        if(pos==s.size())return 0;
+        if(vis[pos][t])return dp[pos][t];
+        vis[pos][t]=1;
+        int ans=0;
+        int lim=t?s[pos]-'0':9;
+        for(int dig=0;dig<=lim;dig++){
+            bool nt= t&&(dig==lim);ll add=0;
+            
+            if(dig==1){
+                ll rem=s.size()-pos-1;ll reman=1;
+                if(!nt){
+                for(int j=0;j<rem;j++)reman*=10;
+                }
+                else{
+                    reman=stoll(s.substr(pos+1))+1;
+                }
+                add=reman;
+
+            }
+            ans+=add+dfs(pos+1,nt);
         }
-        return cnt;
+        return ans;
+    }
+    int countDigitOne(int n) {
+        s=to_string(n);
+        memset(vis,0,sizeof(vis));
+        return dfs(0,true);
     }
 };
