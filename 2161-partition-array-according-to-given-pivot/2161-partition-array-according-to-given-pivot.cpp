ccef63f8b7111120ec4feec5648712e62d700caf
7Navneet7 <navneetsingh19171425@gmail.com>
2026-05-13 11:07:37 +0530

    Time: 9 ms (36.39%), Space: 143.8 MB (6.97%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int piv) {
+        //>piv:0
+        //==piv:1
+        //<piv :2
+        vector<int>parts[3];
+        for(auto it:nums){
+            parts[(it<piv)+(it<=piv)].push_back(it);
+        }
         vector<int>a;
-        for(auto it:nums){
-            if(it<piv)a.push_back(it);
-        }
-        for(auto it:nums){
-            if(it==piv)a.push_back(it);
-        }
-        for(auto it:nums){
-            if(it>piv)a.push_back(it);
+        for(int i=2;i>=0;i--){
+            for(auto it:parts[i])a.push_back(it);
         }
         return a;
     }
