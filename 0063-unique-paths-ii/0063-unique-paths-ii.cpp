4a3626bf41a3aa7059bc64b0c504e4d20688b7bb
7Navneet7 <navneetsingh19171425@gmail.com>
2026-07-07 17:08:26 +0530

    Time: 0 ms (100%), Space: 11.6 MB (78.81%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
-    int util(vector<vector<int>>grid,vector<vector<int>>&dp,int i,int j,int n,int m){
-        if(i>n||j>m)return 0;
-        if(grid[i][j]==1)return 0;
-        if(i==n and j==m)return 1;
-        if(dp[i][j]!=-1)return dp[i][j];
-        int r=util(grid,dp,i,j+1,n,m);
-        int d=util(grid,dp,i+1,j,n,m);
-        return dp[i][j]=r+d;
-    }
-    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
-        int n=grid.size(),m=grid[0].size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return util(grid,dp,0,0,n-1,m-1);
+    int uniquePathsWithObstacles(vector<vector<int>>& mat) {
+        int r=mat.size();int c=mat[0].size();
+        if(mat[0][0]==1 && mat[r-1][c-1]==1)return 0;
+        vector<int>dp(c,0);
+        dp[0]=1;
+        // let's iterate over  the roes
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(mat[i][j]==1){
+                    dp[j]=0;
+                }
+                else if(j>0){
+                    dp[j]=dp[j-1]+dp[j];
+                }
+            }
+        }
+        return dp[c-1];
     }
 };
