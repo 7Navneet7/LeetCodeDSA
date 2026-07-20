77d82b1c9300b1cad68778befa7c22d0ed510934
7Navneet7 <navneetsingh19171425@gmail.com>
2026-07-20 09:04:42 +0530

    Time: 0 ms (100%), Space: 8 MB (38.36%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.cpp
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -1,24 +1,19 @@
 class Solution {
-public:
     using ll=long long;
     ll md=1e9+7;
-    ll power(ll b,ll e){
-        ll r=1;
-        b=b%md;
-        while(e>0){
-            if(e&1){
-                r=(r*b)%md;
-            }
-            b=(b*b)%md;
-            e/=2;
+public:
+    ll power(ll bs,ll ex){
+        if(ex==0)return 1;
+        if(ex==1)return bs;
+        if(ex%2==0){
+            return power((bs*bs)%md,ex/2)%md;
         }
-        return r;
+        return bs*power(bs,ex-1)%md;
     }
     int countGoodNumbers(long long n) {
-        ll epos=(n+1)/2;
-        ll opos=n/2;
-        ll echoi=power(5,epos);
-        ll ochoi=power(4,opos);
-        return (echoi*ochoi)%md;
+        ll evp=(n+1)/2;
+        ll odp=n/2;
+        int ans=(power(5,evp)%md)*(power(4,odp)%md)%md;
+        return ans;
     }
 };
