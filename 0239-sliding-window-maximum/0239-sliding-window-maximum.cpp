5d18919526d3d5ef8c4f29d41aed996b1862333f
7Navneet7 <navneetsingh19171425@gmail.com>
2026-07-22 21:09:02 +0530

    Time: 21 ms (64.58%), Space: 139.2 MB (69.54%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
-    const int maxi=200005;
-    const int logs=18;
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n=nums.size();
-        // st[i]denotes the max element in range starting from i having size 3
-        vector<vector<int>>st(n,vector<int>(logs));
-        vector<int>log(n+1);
-        log[1]=0;
-        for(int i=2;i<=n;i++)log[i]=log[i/2]+1;
+    vector<int> maxSlidingWindow(vector<int>& nu, int k) {
+        int n=nu.size();
+        vector<int>ans;
+        deque<int>dq;
         for(int i=0;i<n;i++){
-            st[i][0]=nums[i];
-        }
-        for(int j=1;j<=logs;j++){
-            for(int i=0;i+(1<<(j))<=n;i++){
-                st[i][j]=max(st[i][j-1],st[i+(1<<(j-1))][j-1]);
+            //if(i>=k-1)ans.push_back(nu[dq.front()]);
+            while(!dq.empty() && nu[dq.back()]<=nu[i]){
+                dq.pop_back();
             }
+            dq.push_back(i);
+            while(!dq.empty() && dq.front()<=i-k)dq.pop_front();
+            if(i>=k-1)ans.push_back(nu[dq.front()]);
         }
-        int j=log[k];vector<int>ans;
-        for(int i=0;i<=n-k;i++){
-            ans.push_back(max(st[i][j],st[i+k-(1<<j)][j]));
 
-        }
         return ans;
-
     }
 };
