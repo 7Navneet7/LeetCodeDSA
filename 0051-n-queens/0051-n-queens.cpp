538c2f63767a3dacb6898a0fa97823cbdbe6f2f4
7Navneet7 <navneetsingh19171425@gmail.com>
2026-07-22 16:32:22 +0530

    Time: 0 ms (100%), Space: 10.1 MB (94.42%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
-    void backT(int r,int n,vector<string>&brd,vector<bool>&cols,
-    vector<bool>&md,vector<bool>&ad,vector<vector<string>>&ans){
-        //base case when all the rows have a queen
+    void backT(int r,int n,vector<string>&brd,vector<bool>&cols,vector<bool>&mainD,
+    vector<bool>&antiD,vector<vector<string>>&ans){
+        //filled all the rows
         if(r==n){
             ans.push_back(brd);return;
         }
-        //trying to place the queen for the current row by trying diff cols
+        //trying columns
         for(int c=0;c<n;c++){
-            int m1=(r-c)+(n-1);
-            int a1=(r+c);
-            // pruning to skip the cols and diags already under attack
-            if(cols[c]||md[m1]||ad[a1])continue;
+            int md=((r-c)+(n-1));
+            int ad=r+c;
+            bool f=cols[c]||mainD[md]||antiD[ad];
+            if(f)continue;
             brd[r][c]='Q';
-            cols[c]=md[m1]=ad[a1]=1;
-            //recurse for next queens placement
-            backT(r+1,n,brd,cols,md,ad,ans);
+            cols[c]=mainD[md]=antiD[ad]=1;
+            //recurse for further rows
+            backT(r+1,n,brd,cols,mainD,antiD,ans);
             brd[r][c]='.';
-            cols[c]=md[m1]=ad[a1]=0;
+            cols[c]=mainD[md]=antiD[ad]=0;
 
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>>ans;
         vector<string>brd(n,string(n,'.'));
-        //look up cheks for queen placement validity
-        vector<bool>cols(n,0);
-        //main diagonal check
-        vector<bool>md(2*n-1,0);
-        //anti diagonal check
-        vector<bool>ad(2*n-1,0);
-        //helper
-        backT(0,n,brd,cols,md,ad,ans);
+        vector<vector<string>>ans;
+        vector<bool>col(n,0);
+        vector<bool>mainD(2*n-1,0);
+        vector<bool>antiD(2*n-1,0);
+        backT(0,n,brd,col,mainD,antiD,ans);
         return ans;
     }
 };
