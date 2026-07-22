bec2bc7ff2316db32777b96a99bb81f58ff32327
7Navneet7 <navneetsingh19171425@gmail.com>
2026-07-22 20:45:41 +0530

    Time: 9 ms (47.64%), Space: 22.1 MB (27.06%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,54 +1,33 @@
 class Solution {
 public:
-    int maximalRectangle(vector<vector<char>>& matrix) {
-        if (matrix.empty() || matrix[0].empty()) return 0;
-
-        int M = matrix.size();
-        int N = matrix[0].size();
-
-        // convert char to int (in-place)
-        vector<vector<int>> mat(M, vector<int>(N));
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                mat[i][j] = matrix[i][j] - '0';
+    int maxAr(vector<int>hts){
+        stack<int>st;
+        hts.push_back(0);
+        int mxa=0;
+        for(int i=0;i<hts.size();i++){
+            while(!st.empty() && hts[st.top()]>hts[i]){
+                int k=st.top();
+                st.pop();
+                int r=i;
+                int l=(st.empty()?-1:st.top());
+                mxa=max(mxa,(r-l-1)*hts[k]);
             }
+            st.push(i);
         }
-
-        // row-wise prefix widths
-        for (int i = 0; i < M; i++) {
-            for (int j = 1; j < N; j++) {
-                if (mat[i][j] == 1) {
-                    mat[i][j] += mat[i][j - 1];
-                }
+        return mxa;
+    }
+    int maximalRectangle(vector<vector<char>>& mat) {
+        int rs=mat.size();int cs=mat[0].size();
+        vector<int>hts(cs,0);
+        int mxa=0;
+        for(int i=0;i<rs;i++){
+            for(int j=0;j<cs;j++){
+                if(mat[i][j]=='1')hts[j]++;
+                else hts[j]=0;
             }
+            mxa=max(mxa,maxAr(hts));
         }
+        return mxa;
 
-        int Ans = 0;
-
-        // fix each column
-        for (int j = 0; j < N; j++) {
-            for (int i = 0; i < M; i++) {
-                int width = mat[i][j];
-                if (width == 0) continue;
-
-                // expand downward
-                int currWidth = width;
-                for (int k = i; k < M && mat[k][j] > 0; k++) {
-                    currWidth = min(currWidth, mat[k][j]);
-                    int height = k - i + 1;
-                    Ans = max(Ans, currWidth * height);
-                }
-
-                // expand upward
-                currWidth = width;
-                for (int k = i; k >= 0 && mat[k][j] > 0; k--) {
-                    currWidth = min(currWidth, mat[k][j]);
-                    int height = i - k + 1;
-                    Ans = max(Ans, currWidth * height);
-                }
-            }
-        }
-
-        return Ans;
     }
 };
