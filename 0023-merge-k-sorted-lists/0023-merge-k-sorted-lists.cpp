e2831488887402d0996f2cc56875d3d247209cf4
7Navneet7 <navneetsingh19171425@gmail.com>
2026-07-23 21:49:24 +0530

    Time: 3 ms (61.55%), Space: 19.1 MB (14.63%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -8,32 +8,28 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
- struct Comparenode{
-    bool operator()(const ListNode* a,const ListNode* b){
-    return a->val > b->val; //min heap creation
-    }
- };
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<ListNode*,vector<ListNode*>,Comparenode>pq;
-        for(auto node:lists){
-            if(node!=nullptr)pq.push(node);
-        }
-        ListNode* dummy=new ListNode(-1);
-        ListNode* tail=dummy;
+        priority_queue<ListNode*,vector<ListNode*>,
+        decltype([](auto &a,auto &b ){
+            return a->val > b->val;
+        })>pq;
+        for(auto &it:lists){
+            if(it!=nullptr)pq.push(it);
+        }    
+        ListNode* ans=new ListNode();
+        ListNode* tail=ans;
+        //ans->next=tail;
         while(!pq.empty()){
-            ListNode* smt=pq.top();
-            tail->next=smt;
+            ListNode *node=new ListNode(pq.top()->val);
+            tail->next=node;
             tail=tail->next;
+            ListNode* curr=pq.top()->next;
             pq.pop();
-            if(smt->next!=nullptr){
-                pq.push(smt->next);
-            }
+            if(curr!=nullptr)pq.push(curr);
         }
-        ListNode* res=dummy->next;
-        delete dummy;
-        return res;
-
+        if(ans->next==nullptr)return nullptr;
+        else return ans->next;
     }
 };
