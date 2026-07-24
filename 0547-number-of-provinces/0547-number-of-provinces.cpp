7ec026a3b59c3fb56c632d63ed4104fb999c1499
7Navneet7 <navneetsingh19171425@gmail.com>
2026-07-24 17:35:01 +0530

    Time: 3 ms (36.73%), Space: 21.5 MB (5.46%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
-    // void dfs(int cur,vector<vector<int>>&adj,vector<bool>&vis){
-    //     vis[cur]=1;
-    //     for(int nei:adj[cur]){
-    //         if(!vis[nei]){
-    //             dfs(nei,adj,vis);
-    //         }
-    //     }
-    // }
     int findCircleNum(vector<vector<int>>& mat) {
-        int v=mat.size();
-        vector<vector<int>>adj(v);
-        for(int i=0;i<v;i++){
-            for(int j=0;j<v;j++){
+        int n=mat.size();
+        vector<vector<int>>adj(n);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
                 if(mat[i][j]==1){
                     adj[i].push_back(j);
                     adj[j].push_back(i);
@@ -20,22 +12,27 @@ public:
             }
         }
         int com=0;
-        vector<bool>vis(v,0);
-        for(int i=0;i<v;i++){
+        vector<bool>vis(n,0);
+        // queue<int>q;q.push(0);
+        // while(!q.empty()){
+        //     int nod=q.front();q.pop();
+        //     for(auto &it:adj[nod]){
+        //         if(!vis[])
+        //     }
+        // }
+        for(int i=0;i<n;i++){
             if(!vis[i]){
                 com++;
-                queue<int>q;
-                q.push(i);vis[i]=1;
+                queue<int>q; q.push(i);
+                vis[i]=1;
                 while(!q.empty()){
-                    int cur=q.front();q.pop();
-                    for(int nei:adj[cur]){
-                        if(!vis[nei]){
-                            vis[nei]=1;
-                            q.push(nei);
+                    int nod=q.front();q.pop();
+                    for(auto &it:adj[nod]){
+                        if(!vis[it]){
+                            q.push(it);vis[it]=1;
                         }
                     }
                 }
-
             }
         }
         return com;
