3ebb70395ea9d0acc3874636aa744e003f911c5e
7Navneet7 <navneetsingh19171425@gmail.com>
2026-07-25 09:42:53 +0530

    Time: 0 ms (100%), Space: 74.6 MB (14.8%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    // int maxSubArray(vector<int>& nums) {
+    int maxSubArray(vector<int>& nu) {
+        int n=nu.size();
+        vector<int>dp(n);
+        dp[0]=nu[0];
+        //dp[i] gives the max subarray sum ending at indx i
+        //choose b/w global choice and local choice
         
-    // }
-    int maxSubArray(vector<int>&nums){
-        int res=nums[0];
-        int total=0;
-        for(int x:nums){
-            if(total<0)total=0;
-            total+=x;
-            res=max(res,total);
+        for(int i=1;i<n;i++){
+            //extend or restart
+            dp[i]=max(nu[i],dp[i-1]+nu[i]);
         }
-        return res;
+        return *max_element(dp.begin(),dp.end());
     }
 };
